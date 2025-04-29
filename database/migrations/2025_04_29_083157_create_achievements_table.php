57e3d91a1353125c86c591822c7b644b754dafae
hothitiep02 <htt301202@gmail.com>
2025-04-29 20:29:52 +0700

    SCRUM 38 | Create migrations for Tasks and Achievements tables- Tiep

---
+++ database/migrations/2025_04_29_083157_create_achievements_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('achievements', function (Blueprint $table) {
+            $table->id();
+            $table->string('image_link')->nullable();
+            $table->string('link')->nullable();
+            $table->text('content');
+            $table->unsignedBigInteger('student_id');
+            $table->string('semester');
+            $table->string('title');
+            $table->timestamps();
+
+            $table->foreign('student_id')->references('id')->on('students')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('achievements');
+    }
+};
