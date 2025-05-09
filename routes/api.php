af4b72c3dd0e9ebbc1a0622bb06ca559c5bb8c74
kimsa-0605 <holykimsa05@gmail.com>
2025-05-10 01:18:54 +0700

    SCRUM-15 | Organize API routes by HTTP method (GET, POST, PUT, DELETE) for better readability - kimsa

--- routes/api.php
+++ routes/api.php
@@ -8,27 +8,37 @@ use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+// GET
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:api');
 
-Route::post('/login', [AuthController::class, 'login']);
 Route::get('/login', function () {
     return response()->json(['message' => 'Unauthorized request'], 401);
 })->name('login');
 
 Route::get('/user/tasks', [TaskController::class, "getAllTasksByStudentId"])
-    ->middleware(['auth:api', 'role:student']);
+->middleware(['auth:api', 'role:student']);
 
 Route::get('/user/achievements', [AchievementController::class, "getAllAchievementsByStudentId"])
-    ->middleware(['auth:api', 'role:student']);
+->middleware(['auth:api', 'role:student']);
 
 Route::get('/user/teacher/classrooms', [ClassroomController::class, "getAllClassroomByTeacherId"])
 ->middleware(['auth:api', 'role:teacher']);
 
 Route::get("/teachers", [UserController::class, "getAllTeachers"])->middleware(['auth:api', 'role:admin']);
 
+Route::get("/admin/students", [UserController::class, "getAllStudents"])->middleware(['auth:api', 'role:admin']);
+
+// POST
+Route::post('/login', [AuthController::class, 'login']);
+
 Route::post("/users", [UserController::class, "createUser"])->middleware(["auth:api", "role:admin"]);
+
 Route::post("/admin/students/bulk", [UserController::class, "createStudents"])->middleware(["auth:api", "role:admin"]);
 
-Route::get("/admin/students", [UserController::class, "getAllStudents"])->middleware(['auth:api', 'role:admin']);
+// PUT
+
+// DELETE
+
+
