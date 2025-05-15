413d2fb0483edf69202257b38327c1f3b204b2c0
kimsa-0605 <holykimsa05@gmail.com>
2025-05-15 10:51:18 +0700

    SCRUM-31 | adjust Classroom and Semester model - kimsa

--- app/Models/Semester.php
+++ app/Models/Semester.php
@@ -13,10 +13,16 @@ class Semester extends Model
         'name',
         'started_at',
         'ended_at',
+        'classroom_id'
     ];
 
     public function semesterGoals()
     {
         return $this->hasMany(SemesterGoal::class);
+    }
+
+    public function classroom()
+    {
+        return $this->belongsTo(Classroom::class);
     }
 }
